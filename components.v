86aefecb0bba6dcd521e8f47f5b4722a64fb173b
kremin1 <mnunn654321@gmail.com>
2025-09-23 14:34:19 +1000

    implemented multiplexer

--- components.v
+++ components.v
@@ -44,15 +44,29 @@ module tick_FSM(rst, clk, enable, tick);
 endmodule
 
 module multiplexer(SignExtDin, R0, R1, R2, R3, R4, R5, R6, R7, G, sel, Bus);
-	/* 
-	 * This module takes 10 inputs and places the correct input onto the bus.
-	 */
-	// TODO: Declare inputs and outputs
-	
-	// TODO: implement logic
+    /* 
+     * This module takes 10 inputs and places the correct input onto the bus.
+     */
+    input [15:0] SignExtDin;
+    input [15:0] R0, R1, R2, R3, R4, R5, R6, R7, G;
+    input [3:0] sel;
+    output reg [15:0] Bus;
 
-
-endmodule
+    always @(*) begin
+        case (sel)
+            4'b0000: Bus = SignExtDin;
+            4'b0001: Bus = R0;
+            4'b0010: Bus = R1;
+            4'b0011: Bus = R2;
+            4'b0100: Bus = R3;
+            4'b0101: Bus = R4;
+            4'b0110: Bus = R5;
+            4'b0111: Bus = R6;
+            4'b1000: Bus = R7;
+            4'b1001: Bus = G;
+            default: Bus = 16'b0;
+        endcase
+    end
 
 module ALU (input_a, input_b, alu_op, result);
     /* 
